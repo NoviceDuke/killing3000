e9f5c19125ab14059fe3a38df03902a64591e6a7
Gimmy <g9308370@hotmail.com>
2020-12-29 12:10:44 +0800

    feat: add a function to pass a variable from frontend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <img @click="getMessage" alt="Vue logo" src="../assets/appicon.png" :style="{ height: '400px' }"/>
+    <img
+      @click="getMessage"
+      alt="Vue logo"
+      src="../assets/appicon.png"
+      :style="{ height: '400px' }"
+    />
     <HelloWorld :msg="message" />
   </div>
 </template>
@@ -10,7 +15,7 @@ import { ref, defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 interface Backend {
-  basic(): Promise<string>;
+  basic(name: string): Promise<string>;
 }
 
 declare global {
@@ -22,19 +27,18 @@ declare global {
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   setup() {
-    
     const message = ref("Click the Icon");
 
     const getMessage = () => {
-      window.backend.basic().then(result => {
+      window.backend.basic("My name").then(result => {
         message.value = result;
       });
-    }
+    };
 
     return { message: message, getMessage: getMessage };
-  },
+  }
 });
 </script>
--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "github.com/leaanthony/mewn"
-  "github.com/wailsapp/wails"
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
 )
 
-func basic() string {
-  return "Hello World!"
+func basic(param string) string {
+	return "Hello World! " + param
 }
 
 func main() {
 
-  js := mewn.String("./frontend/dist/app.js")
-  css := mewn.String("./frontend/dist/app.css")
+	js := mewn.String("./frontend/dist/app.js")
+	css := mewn.String("./frontend/dist/app.css")
 
-  app := wails.CreateApp(&wails.AppConfig{
-    Width:  1024,
-    Height: 768,
-    Title:  "killing3000",
-    JS:     js,
-    CSS:    css,
-    Colour: "#131313",
-  })
-  app.Bind(basic)
-  app.Run()
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:  1024,
+		Height: 768,
+		Title:  "killing3000",
+		JS:     js,
+		CSS:    css,
+		Colour: "#131313",
+	})
+	app.Bind(basic)
+	app.Run()
 }
